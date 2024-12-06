58d26ecec7eac93847cb68f96fea47a6637f75a5
Boyoung Lee <osoon9295@gmail.com>
2024-12-06 17:12:11 +0900

    [PH-1] 로그인 유무에 따른 헤더 변경

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,8 +5,10 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { FaRegBell } from "react-icons/fa6";
+import { useAuth } from "@/hooks/useAuth";
 
 const Header = () => {
+  const { isLogged, handleLogout } = useAuth();
   const [isRead, setIsRead] = useState(true);
   const router = useRouter();
 
@@ -22,15 +24,23 @@ const Header = () => {
           <Image src="/logo.png" alt="로고" width={113} height={20} />
         </Link>
         <div className="flex justify-center items-center gap-3">
-          <div className="flex relative">
-            <button onClick={handleNotificationPage}>
-              <FaRegBell color="#D9D9D9" size="20" />
-            </button>
-            {isRead && <div className="bg-[#F9617A] w-2 h-2 rounded-full absolute right-0 top-0"></div>}
-          </div>
-          <Link href="/login">
-            <button className="w-[50] h-[20] bg-[#FEFFEC]">로그인</button>
-          </Link>
+          {isLogged ? (
+            <>
+              <div className="relative">
+                <button onClick={handleNotificationPage}>
+                  <FaRegBell color="#D9D9D9" size="20" />
+                </button>
+                {isRead && <div className="bg-[#F9617A] w-2 h-2 rounded-full absolute right-0 top-0"></div>}
+              </div>
+              <button onClick={handleLogout} className="w-20 h-8 bg-[#FEFFEC]">
+                로그아웃
+              </button>
+            </>
+          ) : (
+            <Link href="/login">
+              <button className="w-20 h-8 bg-[#FEFFEC]">로그인</button>
+            </Link>
+          )}
         </div>
       </div>
     </header>
