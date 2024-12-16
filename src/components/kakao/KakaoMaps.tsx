1b3ea9d064c6e21a62dd0cb6cc792dcd73d9db5d
[SNGCHN] <[sngchn0312@naver.com]>
2024-12-16 17:13:37 +0900

    [PH-26] 스케줄 페이지 임시

--- src/components/kakao/KakaoMaps.tsx
+++ src/components/kakao/KakaoMaps.tsx
@@ -5,7 +5,7 @@ import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 const KakaoMaps = () => {
   return (
-    <Map center={{ lat: 37.4882, lng: 127.0648 }} style={{ width: "100%", height: "368px" }}>
+    <Map center={{ lat: 37.4882, lng: 127.0648 }} style={{ width: "100%", height: "367px" }}>
       <MapMarker position={{ lat: 37.4882, lng: 127.0648 }} />
     </Map>
   );
