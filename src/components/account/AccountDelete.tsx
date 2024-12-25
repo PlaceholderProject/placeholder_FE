141ed6255712ba1f91f17e5888b7aa47f5f55d66
Boyoung Lee <osoon9295@gmail.com>
2024-12-25 21:33:56 +0900

    [PH-33] 계정 삭제 기능 좀 더 추가

--- src/components/account/AccountDelete.tsx
+++ src/components/account/AccountDelete.tsx
@@ -1,28 +1,27 @@
 "use client";
 
 import { RootState } from "@/stores/store";
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PasswordRecheck from "../auth/PasswordRecheck";
 import Link from "next/link";
 import { deleteUser } from "@/services/user.service";
 import { setIsAuthenticated } from "@/stores/authSlice";
 import Cookies from "js-cookie";
-import { useRouter } from "next/navigation";
 
 const AccountDelete = () => {
+  const [isDeletedAccount, setIsDeletedAccount] = useState(false);
   const isPasswordRechecked = useSelector((state: RootState) => state.auth.isPasswordRechecked);
 
   const dispatch = useDispatch();
-  const router = useRouter();
 
   const handleDeleteUserButton = async () => {
     const response = await deleteUser();
     if (response) {
-      router.replace("/");
       Cookies.remove("accessToken");
       Cookies.remove("refreshToken");
       dispatch(setIsAuthenticated(false));
+      setIsDeletedAccount(true);
     }
   };
 
@@ -33,11 +32,20 @@ const AccountDelete = () => {
           <PasswordRecheck />
         </div>
       )}
-      <p>정말로 탈퇴 하시겠습니까?</p>
-      <Link href="/account">아니요</Link>
-      <button onClick={handleDeleteUserButton} className="bg-slate-400">
-        네
-      </button>
+      {isDeletedAccount ? (
+        <div>
+          <p>탈퇴되었습니다.</p>
+          <Link href="/">메인페이지로 이동하기</Link>
+        </div>
+      ) : (
+        <div>
+          <p>정말로 탈퇴 하시겠습니까?</p>
+          <Link href="/account">아니요</Link>
+          <button onClick={handleDeleteUserButton} className="bg-slate-400">
+            네
+          </button>
+        </div>
+      )}
     </div>
   );
 };
