38e6e83e30dca091112205b6bfe8d320de32420a
JJQKR <yjcoyjco@naver.com>
2025-01-22 17:04:31 +0900

    [PH-118] 페이지네이션 세팅 중

--- src/components/my-space/my-meetup/CurrentMyMeetup.tsx
+++ src/components/my-space/my-meetup/CurrentMyMeetup.tsx
@@ -1,7 +1,131 @@
-import React from "react";
+// import React from "react";
 
-const CurrentMyMeetup = () => {
-  return <div>현재 내 모임 보기</div>;
-};
+// // import {
+// //   // ...
+// //   keepPreviousData,
+// // } from '@tanstack/react-query';
 
-export default CurrentMyMeetup;
+// // const {
+// //   data: postsData,
+// //   isPending,
+// //   isError,
+// // } = useQuery({
+// //     queryKey: ['posts', page],
+// //     queryFn: () => getPosts(page, PAGE_LIMIT),
+// //     placeholderData: keepPreviousData,
+// // });
+
+// const {
+//   data: postsData,
+//   isPending,
+//   isError,
+//   isPlaceholderData,
+// } = useQuery({
+//   queryKey: ["posts", page],
+//   queryFn: () => getPosts(page, PAGE_LIMIT),
+//   placeholderData: keepPreviousData,
+// });
+
+// // ...
+
+// // return (
+// //   ...
+// //     <div>
+// //     <ul>
+// //       {posts.map((post) => (
+// //         <li key={post.id}>{`${post.user.name}: ${post.content}`}</li>
+// //       ))}
+// //     </ul>
+// //     <div>
+// //       <button
+// //         disabled={page === 0}
+// //         onClick={() => setPage((old) => Math.max(old - 1, 0))}
+// //       >
+// //         &lt;
+// //       </button>
+// //       <button
+// //         disabled={isPlaceholderData || !postsData?.hasMore}
+// //         onClick={() => setPage((old) => old + 1)}
+// //       >
+// //         &gt;
+// //       </button>
+// //     </div>
+// //   </div>
+
+// // );
+
+// // ...
+
+// // useEffect(() => {
+// //   if (!isPlaceholderData && postsData?.hasMore) {
+// //     queryClient.prefetchQuery({
+// //       queryKey: ['posts', page + 1],
+// //       queryFn: () => getPosts(page + 1, PAGE_LIMIT),
+// //     });
+// //   }
+// // }, [isPlaceholderData, postsData, queryClient, page]);
+// // ...
+
+// import { useState } from "react";
+// import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+// import { getPosts, uploadPosts, getUserInfo } from "./api";
+
+// const PAGE_LIMIT = 3;
+
+// function HomePage() {
+//   const [page, setPage] = useState(0);
+//   const {
+//     data: postData,
+//     isPending,
+//     isError,
+//   } = useQuery({
+//     queryKey: ["posts", page],
+//     queryFn: () => getPosts(page, PAGE_LIMIT),
+//   });
+
+//   const posts = postsData?.results ?? [];
+
+//   return (
+//     <>
+//       <div>
+//         {currentUserName ? loginMessage : <button onClick={handleLoginButtonClick}>codeit으로 로그인</button>}
+//         <form onSubmit={handleSubmit}>
+//           <textarea name="content" value={content} onChage={handleInputChange} />
+//           <button disabled={!content}>업로드</button>
+//         </form>
+//       </div>
+
+//       <div>
+//         <ul>
+//           {posts.map(post => {
+//             <li key={post.id}>
+//               {post.user.name}: {post.content}
+//             </li>;
+//           })}
+//         </ul>
+//         <div>
+//           <button disabled={page === 0} onClick={() => setPage(old => Math.max(old - 1, 0))}>
+//             &lt;
+//           </button>
+//           <button disabled={!postsData?.hasMore} onClick={() => setPage(old => old + 1)}>
+//             &gt;
+//           </button>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default HomePage;
+
+// const CurrentMyMeetup = () => {
+//   const getCurrentMyMeetupsApi = () => {};
+//   return <div>현재 내 모임 보기</div>;
+// };
+
+// // export async function getPosts(page = 0, limit = 10) {
+// //   const response = await fetch(`${BASE_URL}/posts?page=${page}&limit={limit}`);
+// //   return await response.json();
+// // }
+
+// export default CurrentMyMeetup;
