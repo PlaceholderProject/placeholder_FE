5189b2facc4e3860c06d05a6e83016a003730705
[SNGCHN] <[sngchn0312@naver.com]>
2025-01-23 19:47:13 +0900

    [PH-26] 카카오지도 불러오기 방식 변경

--- src/components/kakao/KakaoMaps.tsx
+++ src/components/kakao/KakaoMaps.tsx
@@ -1,13 +1,37 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Script from "next/script";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 const KakaoMaps = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (window.kakao && window.kakao.maps) {
+      setIsLoaded(true);
+    } else {
+      window.kakao?.maps?.load(() => {
+        setIsLoaded(true);
+      });
+    }
+  }, []);
+
   return (
-    <Map center={{ lat: 37.4882, lng: 127.0648 }} style={{ width: "100%", height: "367px" }}>
-      <MapMarker position={{ lat: 37.4882, lng: 127.0648 }} />
-    </Map>
+    <>
+      <Script
+        type="text/javascript"
+        src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&libraries=services,clusterer&autoload=false`}
+        strategy="afterInteractive"
+        onLoad={() => window.kakao.maps.load(() => setIsLoaded(true))}
+      />
+
+      {isLoaded && (
+        <Map center={{ lat: 37.4882, lng: 127.0648 }} style={{ width: "100%", height: "367px" }}>
+          <MapMarker position={{ lat: 37.4882, lng: 127.0648 }} />
+        </Map>
+      )}
+    </>
   );
 };
 
