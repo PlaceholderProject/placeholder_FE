379c095a56a25bf5866176754447a3f6eaa6f7a8
Boyoung Lee <osoon9295@gmail.com>
2025-04-18 14:59:31 +0900

    [PH-18] 댓글 시간 포맷 주석 추가

--- src/utils/ReplyDateFormat.ts
+++ src/utils/ReplyDateFormat.ts
@@ -1,3 +1,4 @@
+// 현재 시간 0000-00-00 포맷
 const transformCurrentDate = () => {
   const currentToday = new Date();
   const currentYear = currentToday.getFullYear();
@@ -9,6 +10,7 @@ const transformCurrentDate = () => {
   return currentDateString;
 };
 
+// UTC 기준 생성일 -> 댓글 포맷
 export const transformCreatedDate = (date: string) => {
   const createdDate = new Date(date);
   const createdYear = createdDate.getFullYear();
@@ -18,8 +20,8 @@ export const transformCreatedDate = (date: string) => {
   const createdDateString = createdYear + "-" + createdMonth + "-" + createdDay;
 
   if (createdDateString === transformCurrentDate()) {
-    const createdHour = createdDate.getHours();
-    const createdMinute = createdDate.getMinutes();
+    const createdHour = ("0" + createdDate.getHours()).slice(-2);
+    const createdMinute = ("0" + createdDate.getMinutes()).slice(-2);
 
     const createdTimeString = createdHour + ":" + createdMinute;
 
