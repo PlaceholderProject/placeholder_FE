c3fbbad5424d6cf2ae50d6ee3cb9d50874893df8
JJQKR <yjcoyjco@naver.com>
2025-04-19 15:56:53 +0900

    AdButton 코드 주석

--- src/components/ad/AdButton.tsx
+++ src/components/ad/AdButton.tsx
@@ -4,22 +4,22 @@ import React from "react";
 import ProposalPostcard from "../modals/ProposalPostcard";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/stores/store";
-import { toggleProposalPostcardModal } from "@/stores/modalSlice";
+// import { toggleProposalPostcardModal } from "@/stores/modalSlice";
 
 const AdButton = ({ meetupId }: { meetupId: number }) => {
-  const dispatch = useDispatch();
-  const { isProposalPostcardModalOpen } = useSelector((state: RootState) => state.modal);
+  // const dispatch = useDispatch();
+  // const { isProposalPostcardModalOpen } = useSelector((state: RootState) => state.modal);
 
-  const handleProposalModal = () => {
-    dispatch(toggleProposalPostcardModal());
-  };
+  // const handleProposalModal = () => {
+  //   dispatch(toggleProposalPostcardModal());
+  // };
 
   return (
     <>
-      {isProposalPostcardModalOpen && <ProposalPostcard meetupId={meetupId} />}
+      {/* {isProposalPostcardModalOpen && <ProposalPostcard meetupId={meetupId} />}
       <button onClick={handleProposalModal} className="bg-slate-300">
         신청하기
-      </button>
+      </button> */}
     </>
   );
 };
