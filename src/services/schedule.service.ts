ef6d2dd685832f124129e4ba62eb44c274657fd3
[SNGCHN] <[sngchn0312@naver.com]>
2025-04-19 14:57:50 +0900

    [PH-26] 스케줄 수정, 삭제 쿼리 훅 적용

--- src/services/schedule.service.ts
+++ src/services/schedule.service.ts
@@ -22,21 +22,6 @@ export const getSchedules = async (meetupId: number): Promise<Schedule[]> => {
   return data.result;
 };
 
-// 단일 스케줄 가져오기
-export const getSchedule = async (scheduleId: number): Promise<Schedule> => {
-  const token = Cookies.get("accessToken");
-
-  const response = await fetch(`${BASE_URL}/api/v1/schedule/${scheduleId}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-  });
-
-  if (!response.ok) throw new Error("스케줄 조회 실패");
-  return response.json();
-};
-
 //스케줄 생성
 export const createSchedule = async (meetupId: number, formData: FormData): Promise<Schedule> => {
   const token = Cookies.get("accessToken");
@@ -62,3 +47,71 @@ export const createSchedule = async (meetupId: number, formData: FormData): Prom
     throw error;
   }
 };
+
+// 단일 스케줄 가져오기
+export const getSchedule = async (scheduleId: number): Promise<Schedule> => {
+  const token = Cookies.get("accessToken");
+
+  const response = await fetch(`${BASE_URL}/api/v1/schedule/${scheduleId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) throw new Error("스케줄 조회 실패");
+  return response.json();
+};
+
+// 스케줄 수정
+export const updateSchedule = async (scheduleId: number, formData: FormData): Promise<Schedule> => {
+  const token = Cookies.get("accessToken");
+
+  try {
+    const response = await fetch(`${BASE_URL}/api/v1/schedule/${scheduleId}`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error("서버 에러 응답:", errorData);
+      throw new Error(errorData.message || "스케줄 수정 실패");
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error("스케줄 수정 실패:", error);
+    throw error;
+  }
+};
+
+//스케줄 삭제
+export const deleteSchedule = async (scheduleId: number): Promise<void> => {
+  const token = Cookies.get("accessToken");
+
+  try {
+    const response = await fetch(`${BASE_URL}/api/v1/schedule/${scheduleId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "스케줄 삭제 실패");
+    }
+  } catch (err) {
+    console.error("스케줄 삭제 실패:", err);
+    throw err;
+  }
+};
+
+
+
+
+
