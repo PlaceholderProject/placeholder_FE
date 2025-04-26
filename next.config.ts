9012a382b0ef0dabc26b88b81028f0bf5b17280a
[SNGCHN] <[sngchn0312@naver.com]>
2025-04-26 13:59:49 +0900

    [PH-26] next config 수정

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,9 @@
-// 25.04.02 수정 전
-// ⚠ The "images.domains" configuration is deprecated. Please use "images.remotePatterns" configuration instead.
-//next.config.ts 파일에서 domains 설정을 완전히 제거하고 remotePatterns만 사용하도록 수정해야 합니다.
-// 경고 메시지는 domains 설정이 더 이상 권장되지 않는다는 내용이니까요.
-
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   images: {
-//     domains: ["localhost"], // 또는 서버 도메인
-
-//     remotePatterns: [
-//       {
-//         protocol: "http",
-//         hostname: "localhost",
-//         port: "8000",
-//         pathname: "/media/profile_images/**",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    domains: ["localhost"],
+
     remotePatterns: [
       {
         protocol: "http",
@@ -33,12 +11,8 @@ const nextConfig: NextConfig = {
         port: "8000",
         pathname: "/media/profile_images/**",
       },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
-        pathname: "/media/**", // 다른 미디어 경로도 포함하려면
-      },
     ],
   },
 };
+
+export default nextConfig;
