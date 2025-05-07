5b27e2d588f5e856652a96580e4b6fde8830000d
Boyoung Lee <osoon9295@gmail.com>
2025-05-07 15:40:48 +0900

    [PH-177] 검색 form UI 생성

--- src/components/search/SearchArea.tsx
+++ src/components/search/SearchArea.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 
 const SearchArea = () => {
-  return <div>searchArea</div>;
+  const [range, setRange] = useState("ad_title");
+  const [keyword, setKeyword] = useState("");
+
+  const handleRange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setRange(event.target.value);
+  };
+
+  const handleKeyword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(event.target.value);
+  };
+
+  const handleKeywordSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("range", range);
+    console.log("keyword", keyword);
+  };
+
+  return (
+    <div className="bg-[#FBFFA9] p-4">
+      <form onSubmit={handleKeywordSubmit}>
+        <select onChange={handleRange}>
+          <option value="ad_title">제목</option>
+          <option value="organizer">작성자</option>
+          <option value="description">내용</option>
+        </select>
+        <input type="search" onChange={handleKeyword} value={keyword}></input>
+        <button type="submit">
+          <FaSearch />
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SearchArea;
