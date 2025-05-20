e098fa2a4dcf86269e3b1d37ff45a34312123978
JJQKR <yjcoyjco@naver.com>
2025-05-20 18:50:43 +0900

    [PH-213] CSS 설정

--- src/components/modals/ModalLayout.tsx
+++ src/components/modals/ModalLayout.tsx
@@ -10,14 +10,14 @@ interface ModalLayoutProps {
 const ModalLayout = ({ children, onClose }: ModalLayoutProps) => {
   return (
     <div
-      className="fixed inset-0 bg-black/50 flex justify-center items-center z-50"
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
       onClick={onClose} // 바깥 눌렀을 때 닫기
     >
       <div
-        className="bg-white w-[280px] h-[287px] p-6 rounded-xl shadow-lg flex items-center justify-center"
+        className="flex h-[287px] w-[280px] items-center justify-center rounded-xl bg-white p-6 shadow-lg"
         onClick={e => e.stopPropagation()} // 안쪽 누르면 닫기 막기
       >
-        {children}행
+        {children}
       </div>
     </div>
   );
