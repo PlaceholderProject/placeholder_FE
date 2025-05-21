168c9dbf7411f392ff289f15a309dc562177d84a
JJQKR <yjcoyjco@naver.com>
2025-05-21 17:33:33 +0900

    [PH-213] tailwind.config.ts 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,13 @@ const config: Config = {
         pretendard: ["var(--font-pretendard)"],
       },
       screens: {
-        sm: { max: "768px" }, // 모바일 (768px 이하)
-        lg: { min: "769px" }, // PC (769px 이상)
+        // 기존에 sm을 설정해뒀는데,
+        // 그럼 모바일 기본으로 css코딩할 때 매번 sm:을 붙여야 하는 거여서
+        // 아예 삭제하고
+        // 768이하이면 태블릿 사이즈여도 모바일이라고 생각하고 기본 모바일 퍼스트 css, 즉 sm: 없이 코딩,
+        // 769px이면 lg 붙여서 하시면 되겠습니다!
+        // 제가 잘못 이해한 거면 알려주세요^ㅇ^
+        lg: "769px", // PC (769px 이상)
       },
       colors: {
         primary: "#006B8B",
