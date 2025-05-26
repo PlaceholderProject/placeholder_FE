9c7ffaa4777b3f58da362f374925dbe2c82bc00d
SNGCHN <sngchn0312@naver.com>
2025-05-26 11:31:25 +0900

    [PH-257] tailwind.config.ts rem 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,19 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    fontSize: {
+      xs: "1.2rem", // 12px
+      sm: "1.4rem", // 14px
+      base: "1.6rem", // 16px
+      lg: "1.8rem", // 18px
+      xl: "2rem", // 20px
+      "2xl": "2.4rem", // 24px
+      "3xl": "3rem", // 30px
+      "4xl": "3.6rem", // 36px
+      "5xl": "4.8rem", // 48px
+      "6xl": "6.4rem", // 64px
+      "7xl": "7.2rem", // 72px
+    },
     extend: {
       fontFamily: {
         pretendard: ["var(--font-pretendard)"],
