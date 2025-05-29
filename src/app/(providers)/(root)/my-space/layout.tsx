b593e25b1038d7352fe811bb28e026a416cbd362
JJQKR <yjcoyjco@naver.com>
2025-05-29 14:36:03 +0900

    [PH-213] dev 풀 받고 my space layout mt-[10rem]으로 임시 수정

--- src/app/(providers)/(root)/my-space/layout.tsx
+++ src/app/(providers)/(root)/my-space/layout.tsx
@@ -16,21 +16,22 @@ const MySpaceLayout = ({ children }: { children: React.ReactNode }) => {
     <>
       <div>
         <nav>
-          <h1 className="text-xl font-bold m-2">내 공간</h1>
+          <h1 className="m-2 mt-[10rem] text-xl font-bold">내 공간</h1>
+
           <Link
-            className={`rounded-md p-2 m-4 font-semibold ${activeMainTab === "myMeetup" ? "active bg-[#FBFFA9]" : "bg-white"}`}
+            className={`m-4 rounded-md p-2 font-semibold ${activeMainTab === "myMeetup" ? "active bg-[#FBFFA9]" : "bg-white"}`}
             onClick={() => setActiveMainTab("myMeetup")}
             href="/my-space/my-meetup"
           >
             내 모임
           </Link>
-          <Link className={`rounded-md p-2 m-4 font-semibold ${activeMainTab === "myAd" ? "active bg-[#FBFFA9]" : "bg-white"}`} onClick={() => setActiveMainTab("myAd")} href="/my-space/my-ad">
+          <Link className={`m-4 rounded-md p-2 font-semibold ${activeMainTab === "myAd" ? "active bg-[#FBFFA9]" : "bg-white"}`} onClick={() => setActiveMainTab("myAd")} href="/my-space/my-ad">
             내 광고
           </Link>
-          <Link className="rounded-md p-2 m-4 font-semibold" href="/my-space/received-proposal">
+          <Link className="m-4 rounded-md p-2 font-semibold" href="/my-space/received-proposal">
             받은 신청서
           </Link>
-          <Link className="rounded-md p-2 m-4 font-semibold" href="/my-space/sent-proposal">
+          <Link className="m-4 rounded-md p-2 font-semibold" href="/my-space/sent-proposal">
             보낸 신청서
           </Link>
         </nav>
