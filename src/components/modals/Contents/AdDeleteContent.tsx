4fc8663817a4c13d74540306c154da4297cb329a
JJQKR <yjcoyjco@naver.com>
2025-06-12 14:00:16 +0900

    [PH-213]

--- src/components/modals/Contents/AdDeleteContent.tsx
+++ src/components/modals/Contents/AdDeleteContent.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import { BASE_URL } from "@/constants/baseURL";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
