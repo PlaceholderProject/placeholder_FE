7c8afa99ab5643074aea4f9dd067914fd0848862
JJQKR <yjcoyjco@naver.com>
2025-06-14 16:16:30 +0900

    [PH-213]

--- src/components/likes/LikeContainer.tsx
+++ src/components/likes/LikeContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { toggleLikeApi } from "@/services/like.service";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InfiniteData } from "@tanstack/react-query";
