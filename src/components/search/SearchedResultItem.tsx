f62cfc4a1614c4815d2dd95ffea39449733f6a7c
Boyoung Lee <osoon9295@gmail.com>
2025-06-17 12:43:55 +0900

    [PH-186] 작성자 검색결과 시 해당 아이디 굵게 처리

--- src/components/search/SearchedResultItem.tsx
+++ src/components/search/SearchedResultItem.tsx
@@ -9,6 +9,7 @@ const SearchedResultItem = ({ ad }: { ad: SearchedType }) => {
   const { range, keyword } = searchField;
 
   const shouldHighlight = range === "ad_title" && keyword.trim().length > 0;
+  const shouldHighlight2 = range === "organizer" && keyword.trim().length > 0;
 
   const renderTitle = () => {
     if (!shouldHighlight) {
@@ -28,13 +29,31 @@ const SearchedResultItem = ({ ad }: { ad: SearchedType }) => {
     });
   };
 
+  const renderOrganizer = () => {
+    if (!shouldHighlight2) {
+      return <>{ad.organizer.nickname}</>;
+    }
+
+    const escaped = keyword.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
+    const regex = new RegExp(`(${escaped})`, "gi");
+    return ad.organizer.nickname.split(regex).map((part, idx) => {
+      return regex.test(part) ? (
+        <span key={idx} className="font-bold">
+          {part}
+        </span>
+      ) : (
+        <span key={idx}>{part}</span>
+      );
+    });
+  };
+
   return (
     <div className="flex w-[95%] flex-col justify-between border-gray-medium py-[1rem] md:max-w-[111rem]">
       <div className="text-lg">
         <Link href={`/ad/${ad.id}`}>{renderTitle()}</Link>
       </div>
       <div className="flex flex-row justify-between">
-        <div className="flex items-end">{ad.organizer.nickname}</div>
+        <div className="flex items-end text-gray-dark">{renderOrganizer()}</div>
         <div className="flex flex-col items-end text-[#006B8B]">
           <div>{ad.adEndedAt} 까지 모집</div>
           <div className="flex flex-row items-center gap-[0.5rem]">
