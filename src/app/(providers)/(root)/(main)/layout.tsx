842a24673bfdcca71a6cf965e323ef02e25c8a42
JJQKR <105195861+JJQKR@users.noreply.github.com>
2025-06-27 19:30:46 +0900

    Update layout.tsx

--- src/app/(providers)/(root)/(main)/layout.tsx
+++ src/app/(providers)/(root)/(main)/layout.tsx
@@ -5,15 +5,7 @@ import SortArea from "@/components/sort/SortArea";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
-      <div style={{ border: "5px solid red", padding: "20px", margin: "10px" }}>
-        <div>메인 레이아웃 이거 나중에 분리해야되는데 지금은 구조를 맘대로 만질 수 없읍니다</div>
-
-        <SortArea />
-
-        <div>{children}</div>
-      </div>
-    </>
+    <>{children}</>
   );
 };
 
