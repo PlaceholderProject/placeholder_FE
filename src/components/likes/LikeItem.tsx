b1b5bfdedee705ba35d009c2d4e3ee12f2649bd1
JJQKR <yjcoyjco@naver.com>
2025-06-30 17:44:12 +0900

    [PH-213] like 구조 수정

--- src/components/likes/LikeItem.tsx
+++ src/components/likes/LikeItem.tsx
@@ -4,7 +4,7 @@ import { LikeItemProps } from "@/types/likeType";
 import React from "react";
 import { IoMdHeart, IoMdHeartEmpty } from "react-icons/io";
 
-const LikeItem = ({ isLike, likeCount, onClick, disabled = false }: LikeItemProps) => {
+const LikeItem = ({ isLike, likeCount, onToggle, disabled = false }: LikeItemProps) => {
   // return (
   //   <>
   //     <div className="grid grid-cols-2 gap-[0.4rem]">
@@ -20,7 +20,7 @@ const LikeItem = ({ isLike, likeCount, onClick, disabled = false }: LikeItemProp
 
   return (
     <div className="flex items-center gap-[0.4rem]">
-      <button onClick={onClick} disabled={disabled}>
+      <button onClick={onToggle} disabled={disabled}>
         {isLike ? <IoMdHeart className="h-[1.5rem] w-[1.5rem] text-primary" /> : <IoMdHeartEmpty className="h-[1.5rem] w-[1.5rem]" />}
       </button>
       <span className="text-sm">{likeCount}</span>
