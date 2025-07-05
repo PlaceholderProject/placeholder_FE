aca5f785eb5d13c814d6636ae8af53b46cd864cb
JJQKR <yjcoyjco@naver.com>
2025-07-05 16:31:20 +0900

    [PH-213] ❗️baseURL.ts 임시 주석처리

--- src/constants/baseURL.ts
+++ src/constants/baseURL.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = `https://${process.env.NEXT_PUBLIC_BASE_URL}`;
+// export const BASE_URL = `https://${process.env.NEXT_PUBLIC_BASE_URL}`;
+// 로컬 개발환경에서 윗줄은 잠시 주석처리하고 아래 내용을 사용합니다!
+
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
